48b22d7f4a442cdae3476cbe3df49c98c6f54cdc
hardikhtg <63309678+hardikhtg@users.noreply.github.com>
2022-08-13 13:33:27 +0530

    Add files via upload

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,1 +1,28 @@
+const sgMail = require('@sendgrid/mail')
+const sendgridAPIKey = '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' 
 
+sgMail.setApiKey(sendgridAPIKey)
+
+const sendWelcome = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'ameygupta9211@gmail.com',
+        subject: 'Welcome to the app',
+        text: `welcome to using the task app ${name}, lemme know ho it is`
+    }) 
+}
+
+const sendCancel = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'ameygupta9211@gmail.com',
+        subject: 'Goodbye',
+        text: `We'll miss you ${name}, Please tell us how we can improve`
+    }) 
+}
+
+
+module.exports = {
+    sendWelcome,
+    sendCancel
+}
